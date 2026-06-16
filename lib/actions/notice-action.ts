92ccb0e74e234bdd751d6c4bd8f57e9bb2b98772
MostofaSeum <mostofa.seum@brainicontech.com>
2026-06-16 11:28:11 +0600

    CRUD operation for Notice section

---
+++ lib/actions/notice-action.ts
@@ -0,0 +1,77 @@
+import connectDB from "@/lib/db";
+import Notice from "@/lib/models/notice";
+import { success } from "better-auth";
+import { revalidatePath } from "next/cache";
+
+//fetch all notice from database
+export async function getNotice() {
+  try {
+    const notices = await Notice.find({}).sort({ createdAt: -1 });
+    return notices.map((notice) => ({
+      id: notice._id.toString(),
+      name: notice.name,
+      userId: notice.userId,
+      createdAt: notice.createdAt.toString(),
+      updatedAt: notice.updatedAt.toString(),
+    }));
+  } catch (error) {
+    console.log("Error fetching notice:", error);
+  }
+}
+
+//create a new notice
+
+export async function createNotice(name: string, userID: string) {
+  await connectDB();
+  try {
+    const newNotice = await Notice.create({ name, userID });
+    revalidatePath("/dashboard");
+    revalidatePath("/");
+    return {
+      success: true,
+      notice: JSON.parse(JSON.stringify(newNotice)),
+    };
+  } catch (error) {
+    console.log("Error creating notice:", error);
+    return { success: false, error: "Failed to create notice" };
+  }
+}
+
+//update and existing notice
+
+export async function updateNotice(id: string, name: string) {
+  await connectDB();
+  try {
+    const updateNotice = await Notice.findByIdAndUpdate(
+      id,
+      { name },
+      { new: true },
+    );
+    revalidatePath("/dashboard");
+    revalidatePath("/");
+    return {
+      success: true,
+      notice: JSON.parse(JSON.stringify(updateNotice)),
+    };
+  } catch (error) {
+    console.log("Error updating notice:", error);
+    return { success: false, error: "Failed to update notice" };
+  }
+}
+
+
+//delete notice
+export async function deleteNotice(id:string){
+  await connectDB();
+  try{
+    await Notice.findByIdAndDelete(id);
+    revalidatePath("/dashboard");
+    revalidatePath("/");
+    return {
+      success:true,
+    };
+  }catch(error){
+    console.log("Error deleting notice:", error);
+    return { success: false, error: "Failed to delete notice" };
+  }
+}
